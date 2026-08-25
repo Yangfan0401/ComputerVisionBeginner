6d24883ab89fbae78ec18710062cd861c55f25a7
Yangfan0401 <2958193707@qq.com>
2026-08-25 23:06:07 +0800

    alter_rule

--- .history/ImageClassifier/models/softmax_classifier_20260824234039.py
+++
@@ -1,7 +0,0 @@
-
-class Softmax(LinearClassifier):
-  """ A subclass that uses the Softmax + Cross-entropy loss function """
-  def loss(self, W, X_batch, y_batch, reg):
-    return softmax_loss_vectorized(W, X_batch, y_batch, reg)
-
-
